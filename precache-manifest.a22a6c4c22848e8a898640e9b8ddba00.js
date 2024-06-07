c35b54e235b3c46a8af579d154e1c74d36ec7620
Omkar <omkaraher2002@gmail.com>
2024-06-07 21:45:17 +0530

    Updates

---
+++ precache-manifest.a22a6c4c22848e8a898640e9b8ddba00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e3568d0b690d987dabb0bbb80756fa",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/index.html"
+  },
+  {
+    "revision": "35fb93f744b7342f2aff",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/static/css/main.3bdbe1e1.chunk.css"
+  },
+  {
+    "revision": "dde28d145fde4c0af332",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/static/js/2.90eab226.chunk.js"
+  },
+  {
+    "revision": "5a8c50faf123b359b1c0",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/static/js/3.35fc77cc.chunk.js"
+  },
+  {
+    "revision": "35fb93f744b7342f2aff",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/static/js/main.c3646eee.chunk.js"
+  },
+  {
+    "revision": "ebad05900a51e0574211",
+    "url": "%3Comk4567%3E.github.io/%3CDevdynamics-dashboard%3E/static/js/runtime~main.7aba1c84.js"
+  }
+]);
